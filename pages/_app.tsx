4655d154263c377f9baf6c808092f38113637579
danijel_nik <danijel.nikolic@ymail.com>
2022-11-06 22:27:16 +0100

    react-query implementation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import Header from 'components/Header'
 import Navigation from 'components/Navigation/Navigation'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { dehydratedState, ...pageProps } }: AppProps) {
+	const [queryClient] = useState(() => new QueryClient())
+
 	return (
 		<>
-			<Header />
-			<Navigation />
-			<div className="pt-[70px] pl-[250px] w-[100%] pr-[20px] pb-[20px]">
-				<Component {...pageProps} />
-			</div>
+			<QueryClientProvider client={queryClient}>
+				<Hydrate state={dehydratedState}>
+					<Header />
+					<Navigation />
+					<div className="pt-[70px] pl-[250px] w-[100%] pr-[20px] pb-[20px]">
+						<Component {...pageProps} />
+					</div>
+				</Hydrate>
+				<ReactQueryDevtools />
+			</QueryClientProvider>
 		</>
 	)
 }
